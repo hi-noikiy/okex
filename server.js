183fb0ff221baeea8e82c971784bf5f4584a3687
jason.zeng <jasonzvz@gmail.com>
2018-05-13 16:26:57 +0800

    test with binary = 0

--- server.js
+++ server.js
@@ -23,53 +23,72 @@ local_wss.on('connection', function connection(local_ws) {
 
 if (true) {
     // Create a ws to okex server
-    const ws = new WebSocket('wss://okexcomreal.bafang.com:10441/websocket/okcoinapi');
+    //const ws = new WebSocket('wss://okexcomreal.bafang.com:10441/websocket/okcoinapi');
+    const ws = new WebSocket('wss://real.okex.com:10440/websocket/okcoinapi');
     ws.binaryType='arraybuffer';
 
     ws.on('open', function open() {
         // Use binary mode will reduce network loading by 40%
-        ws.send("{'event':'ping'}");
+        //ws.send("{'event':'ping'}");
         //ws.send("{'event':'addChannel','parameters':{'binary': '1', 'type': 'all_ticker_3s'}}");
-        ws.send("{'event':'addChannel','parameters':{'base': 'eos', 'binary': '1', 'product': 'spot', 'quote': 'usdt', 'type': 'ticker'}}");
-        ws.send("{'event':'addChannel','parameters':{'base': 'eos', 'binary': '1', 'product': 'spot', 'quote': 'usdt', 'type': 'depth'}}");
+        //ws.send("{'event':'addChannel','parameters':{'base': 'eos', 'binary': '0', 'product': 'spot', 'quote': 'usdt', 'type': 'ticker'}}");
         //ws.send("{'event':'addChannel','parameters':{'base': 'eos', 'binary': '1', 'product': 'spot', 'quote': 'usdt', 'type': 'deal'}}");
         //ws.send("{'event':'addChannel','parameters':{'base': 'eos', 'binary': '1', 'period': '15min', 'product': 'spot', 'quote': 'usdt', 'type': 'kline'}}");
+        //ws.send("{'event':'addChannel','channel':'ok_sub_futuredusd_eos_ticker_this_week'}");
+        // login (tested but no return value)
+        //ws.send("{'event':'login','parameters':{'api_key':'c2b9cd0b-dd07-4893-b3b6-756d75112a4e','sign': '679DDCD8B471ADB045DA215076130230'}}");
+        ws.send("{'event':'ping'}");
+        //ws.send("{'event':'addChannel','channel':'ok_sub_spot_eos_ticker'}");
+        //ws.send("{'event':'addChannel','channel':'ok_sub_spot_eos_deals'}");
+        //ws.send("{'event':'addChannel','channel':'ok_sub_spot_eos_balance'}");
+        //ws.send("{'event':'addChannel','channel':'ok_sub_futureusd_eos_depth_this_week'}");
+        //ws.send("{'event':'addChannel','parameters':{'base': 'eos', 'binary': '0', 'product': 'spot', 'quote': 'usdt', 'type': 'depth'}}");
     });
 
     ws.on('message', function incoming(message) {
-        try {
-            var array = JSON.parse(message);
-            if (array.event !== undefined) {
-                if (array.event == 'pong') {
-                    g_data.lastHeartBeat = new Date().getTime();
-                    console.log("websocket alive");
-                }
-            }
-        }
-        catch(e) {
-            // Decode the websocket return binary data
-            zlib.inflateRaw(message, function(err, result) {
-                var strData = String.fromCharCode.apply(null, new Uint16Array(result));
-                //var jsonObj = JSON.parse(strData);
-                //g_data = JSON.stringify(processData(jsonObj));
-                //console.log(strData);
-                g_data = strData;
-                local_wss.clients.forEach(function each(client) {
-                    if (client !== ws && client.readyState === WebSocket.OPEN) {
-                        client.send(strData);
-                    }
-                });
-            });
-        }
+        console.log('=================================================');
+        console.log(message);
+        //processData(message);
+        //if (true) {
+        //    var array = message;
+	//    console.log(array);
+        //    if (array.event !== undefined) {
+	//	console.log('event');
+        //    } 
+	//    else if (array.channel !== undefined) {
+	//	console.log('channel');
+        //    }
+	//    else {
+	//	console.log('else');
+        //    }
+        //}
+	//else {
+	//	console.log('Error');
+	//}
+        //catch(e) {
+        //    // Decode the websocket return binary data
+        //    zlib.inflateRaw(message, function(err, result) {
+        //        var strData = String.fromCharCode.apply(null, new Uint16Array(result));
+        //        //var jsonObj = JSON.parse(strData);
+        //        //g_data = JSON.stringify(processData(jsonObj));
+        //        console.log(strData);
+        //        g_data = strData;
+        //        local_wss.clients.forEach(function each(client) {
+        //            if (client !== ws && client.readyState === WebSocket.OPEN) {
+        //                client.send(strData);
+        //            }
+        //        });
+        //    });
+        //}
     });
 
-    setInterval(function() {
-        ws.send("{'event':'ping'}");
-        if ((new Date().getTime() - g_data.lastHeartBeat) > g_data.overtime) {
-            console.log("socket disconnected");
-            //testWebSocket();
-        }
-    }, 5000);
+    //setInterval(function() {
+    //    ws.send("{'event':'ping'}");
+    //    if ((new Date().getTime() - g_data.lastHeartBeat) > g_data.overtime) {
+    //        console.log("socket disconnected");
+    //        //testWebSocket();
+    //    }
+    //}, 5000);
 }
 
 // set the view engine to ejs
